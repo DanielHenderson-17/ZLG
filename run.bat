6e5095d031788739c04e669bcb558e7664235011
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-11-22 13:23:13 -0600

    ya

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-cd "E:/Files/Daniel Henderson Portfolio/Daily" & "C:/Users/djhin/AppData/Local/Programs/Python/Python36-32" "E:/Files/Daniel Henderson Portfolio/Daily/main.py"
+cd "E:/Files/Daniel Henderson Portfolio/Daily" & "C:/Users/djhin/AppData/Local/Programs/Python/Python36-32/python.exe" "E:/Files/Daniel Henderson Portfolio/Daily/main.py"
